586f0236b3dde6cc9241d2db2296b9da7a79a57e
Meherzan <30628872+Meherzan@users.noreply.github.com>
2017-08-06 16:58:53 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one',function(req,res){
-    rees.send('Artile one requested and will be served here');
+    res.send('Artile one requested and will be served here');
 });
 
 app.get('/article-two',function(req,res){
-    rees.send('Artile two requested and will be served here');
+    res.send('Artile two requested and will be served here');
 });
 
 app.get('/article-three',function(req,res){
-    rees.send('Artile three requested and will be served here');
+    res.send('Artile three requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
