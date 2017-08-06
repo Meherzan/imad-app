3617fb89c72db27dc36b92e6dc72302d80d08f0e
Meherzan <30628872+Meherzan@users.noreply.github.com>
2017-08-06 22:44:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -21,7 +21,11 @@ var articleone={
                 </p>`,
 };
 
-
+function createtemplate (data){
+    var title= data.title;
+    var date= data.date;
+    var heading=data.heading;
+    var content= data.content;
 var htmltemplate=`
        <html>
     <head>
@@ -37,33 +41,27 @@ var htmltemplate=`
             </div>
             <hr />
             <h3>
-                Article One
+                ${heading}
             </h3>
             <div>
                 ${date}
             </div>
             <div>
-                <p>
-                    This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.
-                </p>
-                <p>
-                    This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.
-                </p>
-                <p>
-                    This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.This is content of te artcle one.
-                </p>
+               ${content}
             </div>
         </div>
     </body>
 </html>
 `;
+return htmltemplate;
+}
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one',function(req,res){
-      res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+      res.send(createtemplate(articleone));
 });
 
 app.get('/article-two',function(req,res){
