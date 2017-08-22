170239b73466e75da4f9143b2a7e013af364a8f8
Meherzan <30628872+Meherzan@users.noreply.github.com>
2017-08-22 23:19:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var cinfig = {
+    user: 'meherzanturel1997',
+    database: 'meherzanturel1997',
+    host: 'db.imad.hasura-app.io',
+    password:process.env.DB_PASSWORD
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -76,6 +84,19 @@ return htmltemplate;
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,req){
+    //make a select request
+    //return a response with the results
+    pool.query('SELECT * FROM test', function (err, reult){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            res.send(JSON.stringify(result));
+        }
+    })
 });
 
 app.get('/:articlename',function(req,res){
